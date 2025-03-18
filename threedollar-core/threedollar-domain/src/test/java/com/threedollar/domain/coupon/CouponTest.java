a6019d9941797af4e838e328916fa306326e576a
yerimkoko <gochi97@naver.com>
2025-03-19 01:55:25 +0900

    [TDC-160] fix: Coupon domain test 수정

--- threedollar-core/threedollar-domain/src/test/java/com/threedollar/domain/coupon/CouponTest.java
+++ threedollar-core/threedollar-domain/src/test/java/com/threedollar/domain/coupon/CouponTest.java
@@ -8,12 +8,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
-public class CouponTest {
+class CouponTest {
 
     @Test
-    void 쿠폰의_수량이_존재하지_않을때() {
+    void 쿠폰을_사용하면_수량이_한개_감소한다() {
         // given
-        Coupon coupon = getCoupon();
+        Coupon coupon = getValidCoupon();
 
         // when
         coupon.issueCoupon();
@@ -26,7 +26,7 @@ public class CouponTest {
     @Test
     void 유효하지_않은_쿠폰을_사용할_때_에러가_발생한다() {
         // given
-        Coupon coupon = getCoupon();
+        Coupon coupon = getInvalidCoupon();
 
         // when & then
         assertThatThrownBy(coupon::issueCoupon)
@@ -34,7 +34,29 @@ public class CouponTest {
 
     }
 
-    private Coupon getCoupon() {
+    private Coupon getValidCoupon() {
+        String workspaceId = "threedollar-test";
+        String targetId = "USER1";
+        String name = "테스트 쿠폰";
+        CouponType couponType = CouponType.LIMITED;
+        CouponTag couponTag = CouponTag.BOSS_EVENT;
+        CouponGroup couponGroup = CouponGroup.BOSS_STORE;
+        Long count = 1L;
+        String accountId = "1";
+        CouponTime couponTime = new CouponTime(
+            LocalDateTime.of(2025, 3,9,0,0,0),
+            LocalDateTime.of(2099, 3, 10, 0,0,0)
+        );
+
+        return Coupon.newInstance(workspaceId, targetId, name, couponType, couponTag,
+            couponGroup,
+            count,
+            couponTime,
+            accountId);
+
+    }
+
+    private Coupon getInvalidCoupon() {
         String workspaceId = "threedollar-test";
         String targetId = "USER1";
         String name = "테스트 쿠폰";
