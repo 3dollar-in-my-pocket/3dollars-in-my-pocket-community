bfeeb937d6363bc2b6e36b487bfeccae0e2089f4
yerimkoko <gochi97@naver.com>
2025-06-28 12:29:06 +0900

    fix: [TDC-163] add error codes for invalid coupon issue and available periods

--- threedollar-core/threedollar-common/src/main/java/com/threedollar/common/exception/ErrorCode.java
+++ threedollar-core/threedollar-common/src/main/java/com/threedollar/common/exception/ErrorCode.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     E400_INVALID(400, "invalid_request", "잘못된 요청입니다"),
+    E400_INVALID_COUPON_ISSUE_PERIOD(400, "invalid_coupon_issue_time", "쿠폰 발급 가능 시간은 현재 시간보다 이전일 수 없습니다"),
+    E400_INVALID_COUPON_AVAILABLE_PERIOD(400, "invalid_coupon_available_time", "쿠폰 사용 가능 시간은 현재 시간보다 이전일 수 없습니다"),
     E401_INVALID_API_KEY(401, "invalid_api_key", "등록되지 않은 Api-Key 입니다"),
     E404_NOT_FOUND(404, "not_found", "찾을 수 없습니다"),
     E422_UNPROCESSABLE(422, "unprocessable",  "진행할 수 없습니다."),
