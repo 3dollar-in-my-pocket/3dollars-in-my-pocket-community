5d21d62e64bdbb80abcd81f5eac17a2f27ebdd4b
yerimkoko <gochi97@naver.com>
2025-08-11 21:31:05 +0900

    feat: Redis 기반 쿠폰 발급 수 증가 로직 개선
    - 쿠폰 발급 가능 여부 확인 로직 추가
    - 고객이 쿠폰 발급시, 발급한 쿠폰 개수 증가

--- threedollar-application/src/main/java/com/threedollar/service/usercoupon/dto/response/UserCouponService.java
+++ threedollar-application/src/main/java/com/threedollar/service/usercoupon/dto/response/UserCouponService.java
@@ -1,6 +1,7 @@
 package com.threedollar.service.usercoupon.dto.response;
 
 import com.threedollar.common.dto.response.CursorResponse;
+import com.threedollar.common.exception.ConflictException;
 import com.threedollar.common.exception.InvalidException;
 import com.threedollar.common.exception.NotFoundException;
 import com.threedollar.domain.coupon.Coupon;
@@ -9,6 +10,9 @@ import com.threedollar.domain.coupon.CouponUsageStatus;
 import com.threedollar.domain.coupon.repository.CouponRepository;
 import com.threedollar.domain.coupon.repository.UserCouponRepository;
 
+import com.threedollar.domain.coupon.repository.redis.UsedCouponCountRepository;
+import com.threedollar.domain.coupon.repository.redis.couponissuecount.CouponIssueCountKey;
+import com.threedollar.domain.coupon.repository.redis.couponissuecount.CouponIssueCountRepository;
 import com.threedollar.domain.coupon.usercoupon.UserCoupon;
 
 import com.threedollar.service.coupon.CouponServiceHelper;
@@ -27,6 +31,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserCouponService {
 
     private final UserCouponRepository userCouponRepository;
+    private final UsedCouponCountRepository usedCouponCountRepository;
+    private final CouponIssueCountRepository couponIssueCountRepository;
 
     private final CouponServiceHelper couponServiceHelper;
     private final CouponRepository couponRepository;
@@ -76,17 +82,26 @@ public class UserCouponService {
 
         Coupon coupon = couponRepository.findWithLockById(couponId);
         if (coupon == null) {
-            throw new NotFoundException(String.format("쿠폰 (%s) 에 해당하는 couponId 가 존재하지 않습니다.",
-                couponId));
+            throw new ConflictException(String.format("쿠폰 (%s) 에 해당하는 couponId 가 존재하지 않습니다.",
+                couponId.toString()));
         }
 
-        coupon.issueCoupon();
+        // 쿠폰 발급 가능 여부 확인
+        coupon.validateCoupon();
+        if (couponIssueCountRepository.getValueByKey(
+            CouponIssueCountKey.of(couponId, coupon.getWorkspaceId(), coupon.getProviderId())) >= coupon.getMaxCount()) {
+            throw new InvalidException(String.format("쿠폰 (%s) 의 발급 가능 개수(%s) 를 초과했습니다.",
+                couponId, coupon.getMaxCount()));
+        }
 
+        // 고객이 쿠폰 발급
         UserCoupon userCoupon = UserCoupon.newInstance(couponId, workspaceId,
             couponGroup, accountId, coupon.getCouponTime().getAvailableStartTime(),
             coupon.getCouponTime().getAvailableEndTime());
-
         userCouponRepository.save(userCoupon);
+
+        // 발급한 쿠폰 개수 증가
+        couponIssueCountRepository.incrByCount(coupon.getId(), coupon.getWorkspaceId(), coupon.getProviderId());
 
     }
 
