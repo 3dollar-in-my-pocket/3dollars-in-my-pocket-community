cae8a93883506316df4c4c6af06051c8196b7a87
seungh0 <>
2025-12-30 06:05:51 +0900

    feat: migrate from MariaDB to MySQL
    - disable slave connection

--- threedollar-core/threedollar-domain/src/main/java/com/threedollar/config/data/DataSourceConfiguration.java
+++ threedollar-core/threedollar-domain/src/main/java/com/threedollar/config/data/DataSourceConfiguration.java
@@ -1,11 +1,6 @@
 package com.threedollar.config.data;
 
 import com.zaxxer.hikari.HikariDataSource;
-
-import java.util.HashMap;
-import java.util.Map;
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -13,6 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class DataSourceConfiguration {
@@ -23,28 +22,25 @@ public class DataSourceConfiguration {
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.master")
     public DataSource masterDataSource() {
-        return DataSourceBuilder.create()
-            .type(HikariDataSource.class)
-            .build();
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
     }
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.slave")
-    public DataSource slaDataSource() {
-        return DataSourceBuilder.create()
-            .type(HikariDataSource.class)
-            .build();
-    }
+    //    @Bean
+    //    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    //    public DataSource slaDataSource() {
+    //        return DataSourceBuilder.create()
+    //            .type(HikariDataSource.class)
+    //            .build();
+    //    }
 
     @Bean
-    public DataSource routingDataSource(@Qualifier(MASTER_DATASOURCE_NAME) DataSource masterDataSource,
-        @Qualifier(SLAVE_DATASOURCE_NAME) DataSource slaveDataSource) {
+    public DataSource routingDataSource(@Qualifier(MASTER_DATASOURCE_NAME) DataSource masterDataSource) {
 
         ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
 
         Map<Object, Object> dataSourceMap = new HashMap<>();
         dataSourceMap.put("master", masterDataSource);
-        dataSourceMap.put("slave", slaveDataSource);
+        //        dataSourceMap.put("slave", slaveDataSource);
 
         routingDataSource.setTargetDataSources(dataSourceMap);
         routingDataSource.setDefaultTargetDataSource(masterDataSource);
